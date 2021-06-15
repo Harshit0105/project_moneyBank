475ae085419d3e617a7833d4b9fc5a385789183d
Harshit0105 <Harshit010501@gmail.com>
2021-06-15 19:43:45 +0530

    SQLite3

--- project_moneyBank/settings.py
+++ project_moneyBank/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-@xmmd%l41iq((h*!ut^*q0*%gh%)u*k6&v+b4e+rw$372m^c)q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
+# ,'harshit0105.pythonanywhere.com'
 
 
 # Application definition
@@ -76,13 +78,21 @@ WSGI_APPLICATION = 'project_moneyBank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moneybank',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.join(BASE_DIR,'moneybank.sqlite3'),
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'moneybank',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
